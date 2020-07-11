1612802d05684e95c2e857a992a2a7b20ea78a6f
AvidDabbler <walter.k.jenkins@gmail.com>
2020-07-11 14:07:01 -0500

    setup min max dates for slider

--- src/Approval.js
+++ src/Approval.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3';
+import { findByLabelText } from '@testing-library/react';
 
 var w = window.innerWidth,
     h = window.innerHeight
@@ -20,6 +21,40 @@ var svg = d3.select("body").append("svg").attr({
 // .domain([0, d3.max(dataset, function (d) { return d.y + 10; })])
 // .range([margin.top, h - margin.bottom]);  // Set margins for y specific
 
+
+class Slider extends Component {
+    constructor(props) {
+		super(props);
+        this.state = {
+            value: 3
+        };
+        // this.chartRef = React.createRef();
+    }
+    componentDidMount() {
+        this.getInitialState();
+    }
+    getInitialState() {
+        return {value: 3};
+    }
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+    render() {
+        return (
+            <div class="slidecontainer">
+                <input 
+                    id="typeinp" 
+                    type="range" 
+                    min="0" max="5" 
+                    value={this.state.value} 
+                    onChange={this.handleChange.bind(this)}
+                    step="1"/>
+            </div>
+            
+        )
+    }
+}
+
 export default class Approval extends Component{
     constructor(props) {
 		super(props);
@@ -27,7 +62,7 @@ export default class Approval extends Component{
             width: 900,
             height: 450,
         };
-        this.chartRef = React.createRef();
+        // this.chartRef = React.createRef();
     }
     
     async componentDidMount() {
@@ -46,6 +81,10 @@ export default class Approval extends Component{
         }
     };
 
+    setDates() {
+
+    }
+
     getWidth(){
         return this.chartRef.current.parentElement.offsetWidth;
     };
@@ -56,8 +95,16 @@ export default class Approval extends Component{
 
     setData(data) {
         this.setState({ trumpApproval: data });
+        return data;
     };
     
+    setDates(data) {
+        this.setState({
+            minDate: data[1].old_date,
+            maxDate: data[data.length-1].old_date
+        },()=>console.log('state', this.state))
+    }
+
     processData = async () => {
         let csvParse = await d3.csv(await 'https://projects.fivethirtyeight.com/trump-approval-data/approval_topline.csv', data => {
             return {
@@ -70,18 +117,22 @@ export default class Approval extends Component{
         });
         let csvSort = await csvParse.sort((a, b) => a.modeldate - b.modeldate);
         this.setData(await csvSort)
+        this.setDates(this.state.trumpApproval)
+
         console.log('trumpApproval: ', this.state.trumpApproval)
         return await csvSort;
     };
 
+    
+
     //only works with top level element
     //need to find equivelent to document.querySelectorAll('.trump-ratings').target.closest('.approval')
     handleMouseOver(d, i) {  // Add interactivity
-        // Use D3 to select element, change color and size
+        // Use D3 to select element, change color and size          
+        
         d3.select(this)
-            .attr("fill", "orange")
-            .attr("r", d => 10 )
-        console.log('hover')
+            .attr("r", d => 10)
+
     };
 
     handleMouseOut(d, i) {
@@ -93,22 +144,27 @@ export default class Approval extends Component{
     chartRender = async (data, id, div) => {
         const filtered = await data.filter(data => data.subgroup == "All polls");
         const flength = await filtered.length;
+
         let svgChart = d3.select(div)
             .append('svg')
             .attr("width", '100%')
             .attr("height", 450)
             .selectAll('circle')
-            .data(filtered).enter();
-        
-        // svgChart.attr("width", this.state.width)
-        //     .attr("height", this.state.height)
-        //     .attr("class", 'trump-rating')
-
-        // approval render
-        svgChart.append('circle')
             .data(filtered)
             .attr("class", d => id + '_' + d.subgroup)
             .attr("id", d => id + '_' + d.modeldate)
+            .enter();
+        
+        let svgLabel = d3.select(div)
+            .append('div')
+            .style("position", "absolute")
+            .style("z-index", "10")
+            .style("visibility", "hidden")
+            .selectAll('p')
+            .text("a simple tooltip")
+        
+        // approval render
+        svgChart.append('circle')
             .attr("cx", (d, i) => {
                 return (i)*(this.state.width/flength);
             })
@@ -124,9 +180,6 @@ export default class Approval extends Component{
 
         // disapproval render
         svgChart.append("circle")
-            .data(filtered)
-            .attr("class", d => id + '_' + d.subgroup)
-            .attr("id", d => id + '_' + d.modeldate)
             .attr("cx", (d, i) => {
                 return (i)*(this.state.width/flength);
             })
@@ -138,12 +191,28 @@ export default class Approval extends Component{
             .style("fill", d => 'blue')
             .on("mouseover", this.handleMouseOver)
             .on("mouseout", this.handleMouseOut)
+        
+        svgLabel.append('p')
+            .attr("cx", (d, i) => {
+                return (i)*(this.state.width/flength);
+            })
+            .attr("cy", (d) => {
+                const meas = eval( 'd.' + 'disapprove_estimate');
+                return ((100-meas) * (this.state.height/100));
+            })
+            .on('mouseover', () => svg.style('visibility', 'visible'))
+        
     };
+
+
 
     render() {
         // const { width, height, trumpApproval } = this.state; 
         return (
-            <div id='trumpApproval' style={styles.trumpApproval}></div>
+            <div id='approvalContainer'>
+                <Slider />
+                <div id='trumpApproval' style={styles.trumpApproval}></div>
+            </div>
         )
     };
 
@@ -152,9 +221,11 @@ export default class Approval extends Component{
 
 const styles = {
     trumpApproval: {
-        margin: 10,
-        padding: 10,
-        width: '100%',
+        display: 'block',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        justifyContent: 'center',
+        width: '90%',
         height:450
     },
     approval: {

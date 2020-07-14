d78b319bede357a52c99db6d151008b58108be12
AvidDabbler <walter.k.jenkins@gmail.com>
2020-07-13 22:29:03 -0500

    added dates with default values

--- src/Approval.js
+++ src/Approval.js
@@ -1,40 +1,124 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3';
-import { findByLabelText } from '@testing-library/react';
+// import { findByLabelText } from '@testing-library/react';
+// import { Slider, Rail, Handles, Tracks, Ticks } from 'react-compound-slider';
 
 
-class Slider extends Component {
-    constructor(props) {
-		super(props);
-        this.state = {
-            value: 3
-        };
-        // this.chartRef = React.createRef();
-    }
-    componentDidMount() {
-        this.getInitialState();
-    }
-    getInitialState() {
-        return {value: 3};
-    }
-    handleChange(event) {
-        this.setState({value: event.target.value});
-    }
-    render() {
-        return (
-            <div class="slidecontainer">
-                <input 
-                    id="typeinp" 
-                    type="range" 
-                    min="0" max="5" 
-                    value={this.state.value} 
-                    onChange={this.handleChange.bind(this)}
-                    step="1"/>
-            </div>
-            
-        )
-    }
-}
+
+// const sliderStyle = {
+//     position: 'relative',
+//     width: '100%',
+//   }
+  
+//   const defaultValues = [250, 350]
+  
+//   class Example extends Component {
+//     state = {
+//       domain: [200, 500],
+//       values: defaultValues.slice(),
+//       update: defaultValues.slice(),
+//       reversed: false,
+//     }
+  
+//     onUpdate = update => {
+//       this.setState({ update })
+//     }
+  
+//     onChange = values => {
+//       this.setState({ values })
+//     }
+  
+//     setDomain = domain => {
+//       this.setState({ domain })
+//     }
+  
+//     toggleReverse = () => {
+//       this.setState(prev => ({ reversed: !prev.reversed }))
+//     }
+  
+//     render() {
+//       const {
+//         state: { domain, values, update, reversed },
+//       } = this
+  
+//       return (
+//         <div style={{ height: 150, width: '100%' }}>
+//           <button
+//             onClick={() => {
+//               this.onChange([200, 300])
+//               this.onUpdate([200, 300])
+//             }}
+//           >
+//             SET VALUES [200, 300]
+//           </button>
+//           <button
+//             onClick={() => {
+//               this.onChange([350, 450])
+//               this.onUpdate([350, 450])
+//             }}
+//           >
+//             SET VALUES [350, 450]
+//           </button>
+//           <button onClick={() => this.toggleReverse()}>
+//             {reversed ? 'DISPLAY LOW TO HIGH' : 'DISPLAY HIGH TO LOW'}
+//           </button>
+//           <ValueViewer values={values} update={update} />
+//           <Slider
+//             mode={2}
+//             step={1}
+//             domain={domain}
+//             reversed={reversed}
+//             rootStyle={sliderStyle}
+//             onUpdate={this.onUpdate}
+//             onChange={this.onChange}
+//             values={values}
+//           >
+//             <Rail>
+//               {({ getRailProps }) => <SliderRail getRailProps={getRailProps} />}
+//             </Rail>
+//             <Handles>
+//               {({ handles, getHandleProps }) => (
+//                 <div className="slider-handles">
+//                   {handles.map(handle => (
+//                     <Handle
+//                       key={handle.id}
+//                       handle={handle}
+//                       domain={domain}
+//                       getHandleProps={getHandleProps}
+//                     />
+//                   ))}
+//                 </div>
+//               )}
+//             </Handles>
+//             <Tracks left={false} right={false}>
+//               {({ tracks, getTrackProps }) => (
+//                 <div className="slider-tracks">
+//                   {tracks.map(({ id, source, target }) => (
+//                     <Track
+//                       key={id}
+//                       source={source}
+//                       target={target}
+//                       getTrackProps={getTrackProps}
+//                     />
+//                   ))}
+//                 </div>
+//               )}
+//             </Tracks>
+//             <Ticks count={10}>
+//               {({ ticks }) => (
+//                 <div className="slider-ticks">
+//                   {ticks.map(tick => (
+//                     <Tick key={tick.id} tick={tick} count={ticks.length} />
+//                   ))}
+//                 </div>
+//               )}
+//             </Ticks>
+//           </Slider>
+//         </div>
+//       )
+//     }
+//   }
+
 
 export default class Approval extends Component{
     constructor(props) {
@@ -80,14 +164,25 @@ export default class Approval extends Component{
         this.setState({ trumpApproval: data });
         return data;
     };
+  
+    yyyymmdd(date) {
+      var mm = date.getMonth() + 1; // getMonth() is zero-based
+      var dd = date.getDate();
+    
+      return [date.getFullYear(),
+              (mm>9 ? '' : '0') + mm,
+              (dd>9 ? '' : '0') + dd
+            ].join('-');
+    };
+  
     
     setDates(data) {
         data.map(el => {
             el['statDate'] = new Date(el.old_date);
         });
         this.setState({
-            minDate: data[1].statDate,
-            maxDate: data[data.length-1].statDate
+            minDate: this.yyyymmdd(data[1].statDate),
+            maxDate: this.yyyymmdd(data[data.length-1].statDate)
         })
         this.filterDates(data)
     };
@@ -206,9 +301,16 @@ export default class Approval extends Component{
 
     render() {
         // const { width, height, trumpApproval } = this.state; 
+      console.log(this.state)
         return (
             <div id='approvalContainer'>
-                <Slider />
+            {/* <DateSlider /> */}
+            <form>
+              <label>Start Date: </label>
+              <input id='start' type="date" value={this.state.minDate}></input>
+              <label>End Date: </label>
+              <input id='end' type="date" value={this.state.maxDate}></input>
+            </form>
                 <div id='trumpApproval' style={styles.trumpApproval}></div>
             </div>
         )

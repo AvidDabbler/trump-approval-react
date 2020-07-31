392553ca4099858a896f8eba400a4d38a491f660
AvidDabbler <walter.k.jenkins@gmail.com>
2020-07-31 09:10:56 -0500

    update

--- src/private.js
+++ src/private.js
@@ -2,18 +2,11 @@ const p = () => {
     return {
         nyt: 'vfnq95NgVWgHNcN04Hds02ezuzCyOcAn',
         cors: ''
-    }
-
-
-    
+    }    
 };
 
-
-const cors = (url) => {
-    return 'https://mysterious-cove-5444667116.herokuapp.com/' + url + new Date().time;
-}
 const cors_noDate = (url) => {
-    return 'https://mysterious-cove-5444667116.herokuapp.com/' + url;
+    return '' + url;
 };
 
-export { cors, cors_noDate, p };
+export { cors_noDate, p };
